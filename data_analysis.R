e55f5a24168ece4d48a8a514337b8032cf528eca
Michael McGuigan <michaelbmcguigan@gmail.com>
2021-08-08 12:43:29 -0400

    update with intial data cleaning

--- data_analysis.R
+++ data_analysis.R
@@ -1,5 +1,4 @@
-library(tidyverse) # contains ggplot2, dplyr, tidyr, readr, purr, tibble, stringr, forcats
-library(lubridate) # for manipulating dates
+library(tidyverse) # contains ggplot2, dplyr, tidyr, readr, purr, tibble, stringr, forcats, lubridate
 
 # Read the data 
 house_values = read.csv('./data/home_values.csv', header=TRUE, sep = ',')
@@ -21,8 +20,7 @@ house_values <- house_values %>%
     state = State
   )
 house_forecasts <- house_forecasts %>% 
-  rename(rank = SizeRank,
-         zipcode = RegionName,
+  rename(zipcode = RegionName,
          state = StateName, 
          county = CountyName,
          city = CityName, 
@@ -36,7 +34,26 @@ rentals <- rentals %>%
   )
 ## All zip codes are missing leading 0s (where applicable)
 house_values$zipcode <- sapply(house_values$zipcode, function(x){if(nchar(x)<5){paste0(0,x)}else{x}})
-head(house_values$zipcode, 1000)
+house_forecasts$zipcode <- sapply(house_forecasts$zipcode, function(x){if(nchar(x)<5){paste0(0,x)}else{x}})
+rentals$zipcode <- sapply(rentals$zipcode, function(x){if(nchar(x)<5){paste0(0,x)}else{x}})
 
+## Reshape the data so date columns are one per row
+house_values <- pivot_longer(house_values, cols=10:315, names_to = "date", values_to = "price")
+rentals <- pivot_longer(rentals, cols=5:94, names_to = "date", values_to = "price")
 
-  
+## Make dates readable as dates
+house_values$date <- sub(".", "", house_values$date) # remove the leading "X"
+house_values$date <- as_date(house_values$date)
+house_forecasts$date <- as_date(house_forecasts$date) 
+rentals$date <- sub(".", "", rentals$date) # remove the leading "X"
+rentals$date <- parse_date_time(rentals$date, "ym") # convert to date_time so day value is added
+rentals$date <- as_date(rentals$date) # covert to date to drop time value
+rentals$date <- rentals$date-1 # subtract 1 day so values align with other datasets 
+
+## Remove all house values before 2013-12-31 because we do not have a rent value to associate with
+house_values_trim <- house_values %>% filter(date > "2013-12-01")
+
+## Combine to one set of data 
+house_all <- house_values_trim %>% mutate(rent = case_when((house_values_trim$zipcode == rentals$zipcode & 
+                                                              house_values_trim$date == rentals$date) ~ rentals$price)
+  )
